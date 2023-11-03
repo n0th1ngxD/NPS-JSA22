ca5b94983dda80f29a8dfbb4a04ef884e4e986ca
n0th1ngxD <vun9guy3n09@gmail.com>
2023-11-03 20:56:44 +0700

    code

---
+++ SS3/lab2.js
@@ -0,0 +1,91 @@
+// let fruits = [
+//     ["grapes", 15],
+//     ["pineapples", 5],
+//     ["mangos", 10],
+//     ["apples", 20]
+// ]
+
+// // console.log(
+// //     `I have ${fruits[0][1]} ${fruits[0][0]}`
+// // );
+
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(
+//         `I have ${fruits[i][1]} ${fruits[i][0]}`
+//     );
+// }
+
+
+
+
+// //3
+
+
+
+// let myDog = {
+//     "name": "Ngáo",
+//     "legs": 4,
+//     "friends": ["everything!"]
+// };
+
+// myDog.name = "Husky";
+
+// myDog.color = "brown";
+
+// delete myDog.friends;
+
+
+// console.log(myDog);
+
+
+
+
+
+// for (let key in myDog) {
+//     console.log(myDog[key]);
+// }
+
+
+
+
+// let arr = [1, 2, 3, 4, 999];
+// for(let i in arr) {
+//     console.log(arr[i]);
+// }
+
+//NX: i trong for in đại diện cho vị trí phần tử trong mảng
+
+
+
+
+// for - of
+//object
+
+
+// let obj = {
+//     name: "Hiếu",
+//     age: 26,
+//     job: "Web developer"
+// }
+
+
+// for (let key of obj) {
+//     console.log(key);
+// }
+
+
+//array
+
+
+// let arr = [1, 142, 5, 235, 32];
+// for(let key of arr) {
+//     console.log(key);
+// }
+
+//NX: i trong for of đại diện cho giá trị phần tử trong mảng
+
+
+
+
+
